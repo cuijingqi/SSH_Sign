4c7776de46e9219fd7fe1ba25d53c88588dfb28d
cuijingqi <cjq_wy@163.com>
2019-07-22 14:12:03 +0800

    处理样式文件和JS文件加载异常

--- src/main/java/cui/sign/Filter/Power.java
+++ src/main/java/cui/sign/Filter/Power.java
@@ -18,29 +18,35 @@ public class Power implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
+        String reqServletPath = req.getServletPath();
         HttpSession session = req.getSession();
-        boolean isExcludedPage = false;
-        String servletPath = ((HttpServletRequest) servletRequest).getServletPath().replace("//", "");
-        for (String page : excludedPageArray) {//判断是否在过滤url之外
-            if(servletPath.equals(page)){
-                isExcludedPage = true;
-                break;
-            }
-        }
-        if (isExcludedPage){
+        if (reqServletPath.endsWith(".css") || reqServletPath.endsWith(".js")) {
             filterChain.doFilter(servletRequest, servletResponse);
         }else {
-            Admin admin = (Admin) session.getAttribute("admin");
-            if (admin != null && !String.valueOf(admin.getAId()).trim().equals("")) {
-                // 如果现在存在了session，则请求向下继续传递
+            boolean isExcludedPage = false;
+            String servletPath = reqServletPath.replace("//", "");
+            for (String page : excludedPageArray) {//判断是否在过滤url之外
+                if(servletPath.equals(page)){
+                    isExcludedPage = true;
+                    break;
+                }
+            }
+            if (isExcludedPage){
                 filterChain.doFilter(servletRequest, servletResponse);
-            } else {
-                // 跳转到提示登陆页面
-                servletRequest.getRequestDispatcher("/Admin.jsp").forward(servletRequest, servletResponse);
+            }else {
+                Admin admin = (Admin) session.getAttribute("admin");
+                if (admin != null && !String.valueOf(admin.getAId()).trim().equals("")) {
+                    // 如果现在存在了session，则请求向下继续传递
+                    filterChain.doFilter(servletRequest, servletResponse);
+                } else {
+                    // 跳转到提示登陆页面
+                    servletRequest.getRequestDispatcher("/Admin.jsp").forward(servletRequest, servletResponse);
+                }
             }
         }
 
     }
+
 
     @Override
     public void destroy() {
